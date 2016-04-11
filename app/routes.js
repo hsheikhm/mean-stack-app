eb02d854baf45c4ee590ce407b3b7b6a62a40286
Hamza Sheikh <hsheikhm@gmail.com>
2016-04-11 22:16:00 +0100

    created API routes for GET and POST requests

--- app/routes.js
+++ app/routes.js
@@ -4,21 +4,42 @@ var path = require('path');
 
 module.exports = function(app){
 
-  // sample api route
+  // GET all nerds
   app.get('/api/nerds', function(req, res){
-    // use mongoose to get all nerds in the database
     Nerd.find(function(err, nerds){
-      // if there is an error retrieving, send the error.
       if(err){
         res.send(err);
       }
-
       res.json(nerds); // return all nerds in JSON format
     });
   });
 
-  // route to handle creating goes here (app.post)
-  // route to handle delete goes here (app.delete)
+  // CREATE a new nerd
+  app.post('/api/nerds', function(req, res){
+    Nerd.create({
+      name: req.body.name
+    }, function(err, nerd){
+      if(err){
+        res.send(err);
+      }
+
+      Nerd.find(function(err, nerds){
+        if(err){
+          res.send(err);
+        }
+        var responseObject = {
+          message: "Nerd successfully created!",
+          data: nerds
+        };
+        res.json(responseObject);
+      });
+    });
+  });
+
+  // DELETE a nerd
+
+
+
 
   // route to handle all angular requests
   app.get('*', function(req, res){
--- public/js/controllers/MainCtrl.js
+++ public/js/controllers/MainCtrl.js
@@ -1,5 +1,28 @@
-angular.module('MainCtrl', []).controller('MainController', function($scope){
+angular.module('MainCtrl', []).controller('MainController', ['$scope', 'Nerd',
+  function($scope, Nerd){
 
-  $scope.tagline = "To the moon and back!";
+    $scope.tagline = "To the moon and back!";
 
-});
+    Nerd.get()
+      .success(function(data){
+        $scope.allNerds = data;
+      })
+      .error(function(err){
+        console.log('Error: ' + err );
+      });
+
+    $scope.newNerd = function(){
+      Nerd.create({
+        name: $scope.nerdName
+      })
+        .success(function(response){
+          $scope.nerdName = "";
+          $scope.allNerds = response.data;
+          console.log(response.message);
+        })
+        .error(function(err){
+          console.log("Error " + err);
+        });
+    };
+
+}]);
